0a3b14708d5f2cc19a7cd8f843f9d5cebaa91211
mishka31145 <mikh.panfilovv31145@gmail.com>
2025-11-19 11:06:54 +0200

    added HW_4

--- src/main/java/manager/ApplicationManager.java
+++ src/main/java/manager/ApplicationManager.java
@@ -3,13 +3,17 @@ package manager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
+import java.time.Duration;
+
 public class ApplicationManager {
     WebDriver wd;
     HelperUser helperUser;
 
     public void init() {
         wd = new ChromeDriver();
-        wd.navigate().to("https://ilcarro.web.app/search");
+        wd.manage().window().maximize();
+        wd.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
+        wd.navigate().to("https://ilcarro.web.app/");
         helperUser = new HelperUser(wd);
     }
 
--- src/main/java/manager/HelperUser.java
+++ src/main/java/manager/HelperUser.java
@@ -1,10 +1,54 @@
 package manager;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 
 public class HelperUser extends HelperBase {
 
     public HelperUser(WebDriver wd) {
         super(wd);
+    }
+
+    public void openloginForm() {
+        click(By.xpath("//a[text()=' Log in ']"));
+    }
+
+
+    public void fillLoginForm(String email, String password) {
+    type(By.id("email"),email);
+    type(By.id("password"),password);
+    }
+
+    public void submitLogin() {
+        click(By.xpath("//button[@type='submit']"));
+    }
+
+    public String getMessage() {
+//        WebElement element = wd.findElement(By.cssSelector(".dialog-container>h2"));
+//        String text = element.getText();
+//        return text;
+       // pause(2000);
+
+        return wd.findElement(By.cssSelector(".dialog-container>h2")).getText();
+    }
+
+    public void clickOkButton() {
+        By okButtonLocator = By.xpath("//button[text()='Ok']");
+        if (isElementPresent(okButtonLocator)) {
+            click(okButtonLocator);
+        }
+    }
+
+    public boolean isLogged() {
+        return isElementPresent(By.xpath("//*[text()=' Logout ']"));
+    }
+
+    public void logout() {
+        click(By.xpath("//*[text()=' Logout ']"));
+    }
+
+    public String wrongEmailData() {
+        return wd.findElement(By.cssSelector(".error")).getText();
     }
 }
