8c4a4766404c2acf5e853bc3e6c89c74d5deba20
mishka31145 <mikh.panfilovv31145@gmail.com>
2025-11-19 11:06:54 +0200

    added HW_4

--- src/main/java/manager/HelperBase.java
+++ src/main/java/manager/HelperBase.java
@@ -1,6 +1,10 @@
 package manager;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
+import java.util.List;
 
 public class HelperBase {
 
@@ -8,6 +12,32 @@ public class HelperBase {
 
     public HelperBase(WebDriver wd) {
         this.wd = wd;
+    }
+
+    public void click(By locator) {
+        wd.findElement(locator).click();
+    }
+
+    public void type(By locator, String text) {
+        WebElement element = wd.findElement(locator);
+        element.click();
+        element.clear();
+        if(text!=null) {
+            element.sendKeys(text);
+        }
+    }
+
+    public void pause(int time) {
+        try {
+            Thread.sleep(time);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public boolean isElementPresent(By locator) {
+        List<WebElement> list = wd.findElements(locator);
+        return list.size()>0;
     }
 
 }
--- src/test/java/tests/LoginTests.java
+++ src/test/java/tests/LoginTests.java
@@ -1,4 +1,74 @@
 package tests;
 
+import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
 public class LoginTests extends TestBase {
+
+    @BeforeMethod
+    public void preCondition() {
+        if(app.getHelperUser().isLogged()) {
+        app.getHelperUser().logout();
+        }
+    }
+
+
+    @Test
+    public void loginSuccess() {
+    app.getHelperUser().openloginForm();
+    app.getHelperUser().fillLoginForm("mikh.panfilovv31145@gmail.com","Mixan31145$");
+    app.getHelperUser().submitLogin();
+        //Assert --> if element with text "Logged in success" is present
+        Assert.assertEquals(app.getHelperUser().getMessage(),"Logged in success");
+     // app.getHelperUser().clickOkButton();
+
+
+    }
+
+    @Test
+    public void loginSuccessModel() {
+        app.getHelperUser().openloginForm();
+        app.getHelperUser().fillLoginForm("mikh.panfilovv31145@gmail.com","Mixan31145$");
+        app.getHelperUser().submitLogin();
+        //Assert --> if element with text "Logged in success" is present
+        Assert.assertEquals(app.getHelperUser().getMessage(),"Logged in success");
+        //app.getHelperUser().clickOkButton();
+
+
+    }
+
+    @Test
+    public void loginWrongEmail() {
+        app.getHelperUser().openloginForm();
+        app.getHelperUser().fillLoginForm("mikh.panfilovvgmail.com","Mixan31145$");
+        app.getHelperUser().submitLogin();
+
+        Assert.assertEquals(app.getHelperUser().wrongEmailData(),"It'snot look like email");
+    }
+
+    @Test
+    public void loginWrongPassword() {
+        app.getHelperUser().openloginForm();
+        app.getHelperUser().fillLoginForm("mikh.panfilovv31145@gmail.com","Mixan311");
+        app.getHelperUser().submitLogin();
+
+        Assert.assertEquals(app.getHelperUser().getMessage(),"\"Login or Password incorrect\"");
+    }
+
+    @Test
+    public void loginUnregisteredUser() {
+        app.getHelperUser().openloginForm();
+        app.getHelperUser().fillLoginForm("mikh.panfilovv45@gmail.com","Mixan31145$");
+        app.getHelperUser().submitLogin();
+
+        Assert.assertEquals(app.getHelperUser().getMessage(),"\"Login or Password incorrect\"");
+    }
+
+    @AfterMethod
+    public void postCondition() {
+        app.getHelperUser().clickOkButton();
+    }
+
 }
